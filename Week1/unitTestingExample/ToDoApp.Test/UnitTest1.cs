18729e5f14d6136988012a0baa64658c93d1a554
Richard Hawkins <Richard.Hawkins@Revature.com>
2025-09-19 11:04:55 -0400

    add unit testing example

---
+++ Week1/unitTestingExample/ToDoApp.Test/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using ToDoApp;
+
+namespace ToDoApp.Test;
+
+public class MathServiceTests
+{
+    // Arange
+    private readonly IMathService _mathService;
+
+    public MathServiceTests()
+    {
+        _mathService = new MathService();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        // Act
+        var result = _mathService.Square(2);
+
+        // Assert
+        Assert.Equal(4, result);
+    }
+}
