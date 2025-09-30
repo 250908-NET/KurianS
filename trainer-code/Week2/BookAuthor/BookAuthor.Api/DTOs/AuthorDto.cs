9bf149fc79c96cff30fe49304915bd40bda28297
Sneha <snehayur30@gmail.com>
2025-09-30 11:58:03 -0400

    Remove nested trainer-code repository

--- trainer-code/Week2/BookAuthor/BookAuthor.Api/DTOs/AuthorDto.cs
+++
@@ -1,9 +0,0 @@
-namespace BookAuthor.DTOs
-{
-    public class AuthorDto
-    {
-        public int Id { get; set; }      // Author ID
-        public string Name { get; set; } // Author name
-        public string Email { get; set; } // Optional
-    }
-}
